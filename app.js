2f8930d8cef5603325d4c2da785fa8da494648db
fxsla <fuhadulislam02@gmail.com>
2023-08-18 12:43:37 +0100

    Add some functionality

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const myLibrary = [];
+
+
+function addBookToLibrary() {
+    const title = prompt("Please enter a title of the book: ");
+    const author = prompt(`Please enter the author of ${title}`);
+    const pages = prompt(`Please enter the number of pages of ${title}`);
+    const read = prompt(`Have you read the book?`);
+
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
+}
+
+function readBooks(library) {
+    for(book in library){
+        console.log(book);
+    }
+}
+
+
+
+const book = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet" );
+
+
+console.log(book.info())
+
+addBookToLibrary();
+
+readBooks(myLibrary);
